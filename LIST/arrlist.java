820a597bf96d697e4c4c68f5fa3abf712b54f248
Sujal-Shreshya <ishub515@gmail.com>
2026-02-27 11:01:03 +0530

    updated code

---
+++ LIST/arrlist.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class arrlist {
+    public static void main(String args[]) {
+        ArrayList<Integer> car = new ArrayList<>();
+        car.add(1);
+        car.add(2); 
+        car.add(3);
+        car.add(4);
+        car.remove(2);
+        
+        System.out.println(car);
+        }
+    }
+
---
+++ reverse_no.java
@@ -0,0 +1,12 @@
+public class reverse_no {
+    public static void main(String[] args) {
+        int num = 12345, reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Reversed number: " + reversed);
+    }
+    
+}
